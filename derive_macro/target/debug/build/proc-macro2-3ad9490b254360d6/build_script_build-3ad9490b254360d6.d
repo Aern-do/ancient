104ed01abdc1e07508931720245c2566949488df
Raddeonn <radeonchik.new@gmail.com>
2023-04-16 22:32:34 +0300

    fix: .gitignore fix

--- derive_macro/target/debug/build/proc-macro2-3ad9490b254360d6/build_script_build-3ad9490b254360d6.d
+++
@@ -1,5 +0,0 @@
-c:\Users\radeo\OneDrive\Робочий стіл\projects\ancient\derive_macro\target\debug\build\proc-macro2-3ad9490b254360d6\build_script_build-3ad9490b254360d6.exe: C:\Users\radeo\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.56\build.rs
-
-c:\Users\radeo\OneDrive\Робочий стіл\projects\ancient\derive_macro\target\debug\build\proc-macro2-3ad9490b254360d6\build_script_build-3ad9490b254360d6.d: C:\Users\radeo\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.56\build.rs
-
-C:\Users\radeo\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.56\build.rs:
--- derive_macro/target/debug/deps/derive_macro-03414c7f8b4d8ea6.d
+++
@@ -1,6 +0,0 @@
-c:\Users\radeo\OneDrive\Робочий стіл\projects\ancient\derive_macro\target\debug\deps\derive_macro-03414c7f8b4d8ea6.rmeta: src\lib.rs src\input\mod.rs
-
-c:\Users\radeo\OneDrive\Робочий стіл\projects\ancient\derive_macro\target\debug\deps\derive_macro-03414c7f8b4d8ea6.d: src\lib.rs src\input\mod.rs
-
-src\lib.rs:
-src\input\mod.rs:
